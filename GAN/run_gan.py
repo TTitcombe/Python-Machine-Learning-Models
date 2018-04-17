1bd64219f9dbe4fd3a603a81adbcea0339ab14f1
Tom <32938439+TTitcombe@users.noreply.github.com>
2018-04-17 17:06:17 +0100

    Command line running
    Add argument parser so model can be run from command line

--- GAN/run_gan.py
+++ GAN/run_gan.py
@@ -1,7 +1,16 @@
 import numpy as np
 import cv2
+import argparse
 
 from gan import GAN
+
+def take_samples(model, N_samples, path='./generated_im'):
+    anImg = gan.sample()
+    for i in range(N_samples):
+        path = path + str(i) + '.jpg'
+        cv2.imwrite(path, anImg[i,:,:]*255)
+        
+ 
 
 hyperparams = {}
 hyperparams['lr'] = 0.001
@@ -14,7 +23,23 @@ gan = GAN(784, [128],[128],100,1, hyperparams)
 gan.train()
 
 N_samples = 3 #must be less than batch size
-anImg = gan.sample()
-for i in range(N_samples):
-    path = "./generated_im{}.jpg".format(i)
-    cv2.imwrite(path, anImg[i,:,:]*255)
+take_samples(gan, N_samples)
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Basic MNIST Gan')
+    parser.add_argument('--digit', type=int, default=2)
+    parser.add_argument('--N_samples', type=str, default='1')
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batchSize', type=int, default=64)
+    args = parser.parse_args()
+    
+    hyperparams = {'lr' = args.lr, 
+                   'epochs' = args.epochs, 
+                   'batchSize' = args.batchSize,
+                   'number' = args.digit}
+    
+    gan = GAN(784, [128], [128], 100, 1, hyperparams)
+    gan.train()
+    
+    take_samples(gan, args.N_samples)
