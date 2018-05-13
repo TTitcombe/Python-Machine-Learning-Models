25b0cace783bc6aaef1de8e9a2cc5ec2a877c71e
tjt216 <tjt216@ic.ac.uk>
2018-05-13 19:56:50 +0100

    refactoring of RBM

--- RBM/rbm.py
+++ RBM/rbm.py
@@ -4,8 +4,8 @@ import cv2
 class RBM(object):
     """An RBM Object"""
 
-    def __init__(self,nInput, nHid, archs):
-        self.nInput = nInput
+    def __init__(self,nVis, nHid, archs):
+        self.nVis = nVis
         self.nHid = nHid
         self.archs = archs
         """Archs:
@@ -19,95 +19,79 @@ class RBM(object):
         self.epochs = archs.get('epochs', 100)
         self.k = archs.get('k', 1)
 
-        weights, b, c = self.random_init(nInput, nHid)
-        self.weights = weights
+        W, b, c = self.random_init(nVis, nHid)
+        self.W = W
         self.b = b
         self.c = c
 
     @staticmethod
-    def random_init(nInput, nHid, seed=None):
-        if seed:
-            np.random.seed(seed)
-        weights = np.random.randn(nInput, nHid)
-        b = np.zeros((nInput))
+    def random_init(nVis, nHid, seed=None):
+        if seed is None:
+            seed = np.random.RandomState(42)
+        W = np.asarray(seed.uniform(
+        low=-4 * np.sqrt(6. / (nHid + nVis)),
+        high=4 * np.sqrt(6. / (nHid + nVis)),
+        size=(nVis, nHid)))
+        b = np.zeros((nVis))
         c = np.zeros((nHid))
-        return weights, b, c
+        return W, b, c
 
-    def train(self, x_train):
-        batches = len(x_train) // self.batchSize
-        for epoch in range(self.epochs):
-            for batch in range(batches):
-                dw = np.zeros((self.nInput,self.nHid))
-                for i in range(self.batchSize):
-                    x = x_train[batch*self.batchSize + i]
-                    posX = self.sample(x,None,True)
-                    posX_ = np.outer(x, posX)
+    def negative(self, hidden):
+        new_vis, vis_binary = self.sample_nodes(hidden, False)
+        new_hidden, hidden_binary = self.sample_nodes(vis_binary, True)
 
-                    #CD-1
-                    newVis = self.sample(None,posX, False)
-                    negX = self.sample(newVis,None, True)
-                    negX_ = np.outer(newVis, negX)
+        return new_hidden, hidden_binary, new_vis, vis_binary
 
-                    dw += (posX_ - negX_)
-
-                print('Epoch: {}; Batch: {}; Weight mean: {}'.format(epoch, batch, np.mean(self.weights)))
-
-                dw = dw / self.batchSize
-                self.weights = self.weights + self.lr * dw #gradient ascent
-                image = self.sample_image()
-                image = np.reshape(image, (28,28))
-                cv2.imshow('Image', image)
-                cv2.waitKey(1)
-
-
-    def sample(self,input_data,hidden_data, hidden):
-        '''Block gibbs sampling'''
+    def sample_nodes(self, nodes, hidden):
+        #New function
+        '''Sample a new layer given input nodes.
+        hidden is a boolean value. If True, new hidden being calculated.'''
         if hidden:
-            newHid = np.matmul(input_data, self.weights)
-            newHid = self.prob(input_data, newHid, 100.0,hidden)
-            newLayer = newHid
+            new_layer = np.matmul(nodes, self.W) + self.c
         else:
-            newInp = np.matmul(self.weights,hidden_data)
-            newInp = self.prob(newInp, hidden_data, 100.0,hidden)
-            newLayer = newInp
+            new_layer = np.matmul(nodes, self.W.T) + self.b
 
-        newLayer[newLayer >= 0.5] = 1
-        newLayer[newLayer < 0.5] = 0
-        return newLayer
+        layer_binary = np.zeros(new_layer.shape)
+        layer_binary[new_layer >= 0.5] = 1
 
-    def prob(self,inp,hid,T,hidden):
-        '''Calculate prop of a variable = 1 given the other variables.
-        inputs:
-            inp | nodes in input layer, np array
-            hid | nodes in input layer, np array
-            T | boltzmann temperature
-            hidden | bool, true if calculating hidden prop
-        '''
-        if hidden:
-            exp = np.exp(-self.h(inp,hidden) * hid / T)
-            return 1/(1+exp)
-        else:
-            exp = np.exp(-self.h(hid,hidden) * inp / T)
-            return 1/(1+exp)
+        return new_layer, layer_binary
 
-    def h(self,nodes, hid):
-        '''Calculate a layer in the rbm.
-        inputs:
-            nodes | the nodes in the layer, either v or h
-            hid | boolean value. True if calculating new hidden layer.
-        '''
-        if hid:
-            return np.matmul(nodes, self.weights) #+ self.c
-        else:
-            return np.matmul(self.weights,nodes) #+ self.b
+    def train(self,x_train, k=1):
+            batches = len(x_train) // self.batchSize
+            for epoch in range(self.epochs):
+                for batch in range(batches):
+                        input = x_train[batch*self.batchSize: (batch+1)*self.batchSize]
+
+                        pos_h, hidden_binary = self.sample_nodes(input, True) #how to get hidden nodes
+
+                        for i in range(k):
+                            hidden, hidden_binary, vis, vis_binary = self.negative(hidden_binary)
+
+                        #W update
+                        pos_phase = np.dot(input.T, pos_h)
+                        neg_phase = np.dot(vis_binary.T, hidden)
+
+                        self.W += self.lr * (pos_phase - neg_phase)
+                        self.b += self.lr * np.mean(input - vis_binary, axis=0)
+                        self.c += self.lr * np.mean(pos_h - hidden, axis = 0)
+
+                        print("Epoch: {}; Batch: {}".format(epoch, batch))
+                        image = self.sample_image()
+                        image = np.reshape(image, (28,28))
+                        cv2.imshow('Image', image)
+                        cv2.waitKey(1)
+
+    @staticmethod
+    def sigmoid(x):
+        return 1. / (1. + np.exp(-x))
 
     def sample_image(self):
         image = self.generate()
         return image * 255
 
     def generate(self):
-        newInputs = np.random.randint(0,2,self.nInput)
+        newVis = np.random.randint(0,2,self.nVis)
         for i in range(100):
-            newHid = self.sample(newInputs,None,True)
-            newInputs = self.sample(None,newHid, False)
-        return newInputs
+            _, newHid = self.sample_nodes(newVis, True)
+            _, newVis = self.sample_nodes(newHid, False)
+        return newVis
--- RBM/run_rbm.py
+++ RBM/run_rbm.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from rbm import RBM
+
 from utils import load_mnist_binary
 
 data, _ = load_mnist_binary(2)
